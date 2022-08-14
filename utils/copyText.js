7eea163bc44eb84e60c92bd7e58f78ae667f830b
Moises Barillas <moisesrocha717@gmail.com>
2022-08-13 20:13:15 -0600

    remove util file

--- utils/copyText.js
+++
@@ -1,8 +0,0 @@
-export const copyToClipboard = async (text) => {
-	const input = document.createElement('input');
-	input.value = text;
-	document.body.appendChild(input);
-	input.select();
-	document.execCommand('copy');
-	document.body.removeChild(input);
-};
